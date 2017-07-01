8533c53749f45c0ff3293e8c46494c01915be064
徐敏辉 <xuminhui189@gmail.com>
2017-07-01 14:02:18 +0800

    add Block & EditModal component

---
+++ app/views/Block.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Block extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
