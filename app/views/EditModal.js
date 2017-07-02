4e82954a9314287985186a2705553172fabc946b
徐敏辉 <xuminhui189@gmail.com>
2017-07-02 19:48:41 +0800

    make editmodal editable

--- app/views/EditModal.js
+++ app/views/EditModal.js
@@ -21,7 +21,10 @@ export default class EditModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modalVisible: false
+            modalVisible: false,
+            url: props.url,
+            title: props.title,
+            content: props.content
         };
     }
 
@@ -44,17 +47,16 @@ export default class EditModal extends Component {
         this.setState({ modalVisible }, () => {
             // callback function
             this.props.cb();
+            console.log(this.state.title);
         });
     }
 
     cancelClick(modalVisible) {
         this.setState({ modalVisible }, () => {
             // TODO some cleanup
-            this.props.cb();
+            const { url, title, content } = this.state;
+            this.props.cb(url, title, content);
         });
-    }
-
-    handleInputChange() {
     }
 
     componentWillReceiveProps(ntProps) {
@@ -82,22 +84,38 @@ export default class EditModal extends Component {
                     <FormItem>
                         <Input
                             type='text'
-                            value={this.props.url}
+                            value={this.state.url}
+                            onChange={(e) => {
+                                this.setState({
+                                    url: e.target.value
+                                });
+                            }}
                         ></Input>
                     </FormItem>
                     <FormItem label='Title'></FormItem>
                     <FormItem>
                         <Input
                             type='text'
-                            value={this.props.title}
+                            value={this.state.title}
+                            onChange={(e) => {
+                                this.setState({
+                                    title: e.target.value
+                                });
+                            }}
                         ></Input>
                     </FormItem>
                     <FormItem label='Content'></FormItem>
                     <FormItem>
                         <Input
                             type='textarea'
+                            /* textarea size */
                             autosize={{ minRows: 4, maxRows: 10 }}
-                            value={this.props.content}
+                            value={this.state.content}
+                            onChange={(e) => {
+                                this.setState({
+                                    content: e.target.value
+                                });
+                            }}
                         ></Input>
                     </FormItem>
                 </Form>
