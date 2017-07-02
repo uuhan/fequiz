1e5eb462d853bf43a09afaa7ffbd4abdb83c7aab
徐敏辉 <xuminhui189@gmail.com>
2017-07-02 14:21:05 +0800

    add local Block style

--- app/views/Block.js
+++ app/views/Block.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import { Card, Button }             from 'antd';
+import PropTypes            from 'prop-types';
 
+/** use local stylesheet */
+import {hoverBtn}           from './Block.css';
+
+/**
+ * Block: A Block for showing an article
+ *
+ * @export
+ * @class Block
+ * @extends {Component}
+ */
 export default class Block extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            /** @type {boolean} check if mouse hovered */
+            hovered: true
+        }
+    }
+
+    static propTypes = {
+        /** @type {string} the image url */
+        url: PropTypes.string.isRequired,
+        /** @type {string} the title of an article */
+        title: PropTypes.string.isRequired,
+        /** @type {string} the content of an article */
+        content: PropTypes.string.isRequired
     }
 
     componentDidMount() {
     }
 
     render() {
-        return (
-            <div>
+        return(
+            <div style={{ padding: 0, position: 'relative', textAlign: 'left'}}>
+                <Button size='large' type='primary' className={hoverBtn}>
+                    Edit
+                </Button>
+                <div style={{padding: '20px', backgroundColor: 'white'}}>
+                    <Card style={{ width: '100%' }} bodyStyle={{ padding: 0 }} bordered={false}>
+                        <div className="custom-image">
+                            <img alt="" width="100%" src={this.props.url} />
+                        </div>
+                        <div className="custom-card">
+                            <h2>{this.props.title}</h2>
+                            <p>{this.props.content}</p>
+                        </div>
+                    </Card>
+                </div>
             </div>
         )
     }
--- app/views/Mainpage.js
+++ app/views/Mainpage.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Button, Row, Col } from 'antd';
+
+import Block                from './Block';
 
 export default class Mainpage extends Component {
     constructor(props) {
@@ -6,15 +9,32 @@ export default class Mainpage extends Component {
     }
 
     componentDidMount() {
-        console.log('place holder');
     }
 
     render() {
         return (
-            <div>
-                placeholder
-                { this.props.children }
-            </div>
+            <Row style={{marginTop: '60px', backgroundColor: '#f2f2f2'}}>
+                <Col span={2}/>
+                <Col span={14}>
+                    {/* List Blocks Here */}
+                    <Block
+                        url='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                        title='Europe Street beat'
+                        content='Dolor proident quis cillum anim ea officia anim ex culpa minim occaecat et. Irure laborum dolore consequat voluptate nulla magna Lorem ad. Ex sunt enim officia id consequat occaecat. Aute tempor in laboris dolore mollit in id culpa culpa amet veniam pariatur nisi. Esse reprehenderit ut amet et minim eiusmod officia eu. Deserunt Lorem ea eiusmod esse. Et quis est consectetur Lorem ad tempor duis quis voluptate aliqua occaecat ipsum.'
+                    />
+                </Col>
+                <Col span={2}/>
+                <Col span={4}>
+                    <Button
+                        type='primary'
+                        size='large'
+                        style={{
+                            width: '100%',
+                            backgroundColor: '#5fb485'
+                        }}>+ Add New</Button>
+                </Col>
+                <Col span={2}/>
+            </Row>
         )
     }
 }
